f53c78b04bf4832950af15600ed21ff70d3d2a57
Yuliia Kravchenko <yukravch@f6r12s15.clusters.42paris.fr>
2025-05-13 12:48:48 +0200

    Finished explaining readline functions and open/read/close directory

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: yukravch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 09:55:30 by yukravch          #+#    #+#             */
-/*   Updated: 2025/05/13 10:40:29 by yukravch         ###   ########.fr       */
+/*   Updated: 2025/05/13 12:35:21 by yukravch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,12 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <string.h>
 # include <signal.h>
 # include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <sys/types.h>
+# include <dirent.h>
 
 #endif
