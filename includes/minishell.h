9366a377a1e3a2fb47a36d8cc36048eb58ef1d0c
Lyes Fournier <lfournie@f6r6s1.clusters.42paris.fr>
2025-05-19 14:41:14 +0200

    Started simple parsing

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,25 +6,29 @@
 /*   By: lfournie <lfournie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 09:55:30 by yukravch          #+#    #+#             */
-/*   Updated: 2025/05/16 14:07:52 by yukravch         ###   ########.fr       */
+/*   Updated: 2025/05/19 14:36:34 by lfournie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 #define MINISHELL_H
 
-typedef struct s_quote
-{
-	int	sp;
-	int	db;	
-}			t_quote;
-
 typedef struct s_token
 {
 	char	*token;
 	int		type;
-	void	*next;	
+	void	*next;
 }			t_token;
+
+typedef	enum s_type
+{
+	CMD,
+	PIPE,
+	HEREDOC,
+	INPUT,
+	OUTPUT,
+	APPEND,
+}			t_type;
 
 # include <unistd.h>
 # include <stdio.h>
--- sources/main.c
+++ sources/main.c
@@ -2,42 +2,48 @@
 #include "minishell.h"
 
 
-bool	ft_check_quotes(char *str)
+static bool	ft_check_unclosed_quotes(char *str)
 {
 	int 	i;
-	t_quote	quote;
+	int		sp;
+	int		db;
 
 	i = 0;
-	quote.sp = 0;
-	quote.db = 0;
+	sp = 0;
+	db = 0;
 	while (str[i])
 	{
-		if ((str[i] == 34 && quote.sp != 0) || (str[i] == 39 && quote.db != 0))
-			return (false);
-		if (str[i] == 39)
-			quote.sp++;
-		else if (str[i] == 34)
-			quote.db++;
+		if (str[i] == 39 && db == 0)
+			sp++;
+		else if (str[i] == 34 && sp == 0)
+			db++;
+		if (sp % 2 == 0)
+			sp = 0;
+		if ( db % 2 == 0)
+			db = 0;
 		i++;
 	}
-	if (quote.sp % 2 != 0 || quote.db % 2 != 0)
+	if (sp % 2 != 0 || db % 2 != 0)
 		return (false);
 	return (true);
 }
+static 
 
-int main()
+
+int main(int ac, char **av)
 {
 	char* line;
-	
+	(void)av;
+
+	if (ac != 1)
+		return(0);
+	printf("%s\n", getenv("PATH"));
 	while (1)
 	{
 		line = readline("babyshell: ");
-		if (ft_check_quotes(line))
-			printf ("All is good my dood\n");
-		else
-			printf("quotes no good ouinouin\n");
-		if (line[0] == 'C')
-			exit(EXIT_SUCCESS);
+		if (!ft_check_unclosed_quotes(line))
+			return (1);
+		
 		free(line);
 	}
 	return (0);
