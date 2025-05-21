e9bed64a50bbb9a7dce0663f3f7ddffce0200ab0
Yuliia Kravchenko <yukravch@f6r6s3.clusters.42paris.fr>
2025-05-21 14:41:30 +0200

    Optimized checking for in build commands

--- sources/execution/execution_Y.c
+++ sources/execution/execution_Y.c
@@ -3,39 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   execution_Y.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yukravch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: yukravch <yukravch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 13:47:36 by yukravch          #+#    #+#             */
-/*   Updated: 2025/05/21 13:53:24 by yukravch         ###   ########.fr       */
+/*   Updated: 2025/05/21 14:41:04 by yukravch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "minishell.h"
 
-char	*ft_check_if_build_in_cmd(t_token *token)
+const char	*ft_check_if_build_in_cmd(t_token *token)
 {
-	if (ft_strncmp(token->value, "echo", ft_strlen(token->value)) == 0)
-		return ("echo");
-		//call ft to execute build in command
-	if (ft_strncmp(token->value, "cd", ft_strlen(token->value)) == 0)
-		return ("cd");
-	if (ft_strncmp(token->value, "pwd", ft_strlen(token->value)) == 0)
-		return ("pwd");
-	if (ft_strncmp(token->value, "export", ft_strlen(token->value)) == 0)
-		return ("export");
-	if (ft_strncmp(token->value, "unset", ft_strlen(token->value)) == 0)
-		return ("unset");
-	if (ft_strncmp(token->value, "env", ft_strlen(token->value)) == 0)
-		return ("env");
-	if (ft_strncmp(token->value, "exit", ft_strlen(token->value)) == 0)
-		return ("exit");
-	return (0);
+	int			i;
+	const char	*build_in_names[] = {"echo", "cd", "pwd",
+		"export", "unset", "env", "exit"};
+
+	i = 0;
+	while (i < 7)
+	{
+		if (ft_strncmp(token->value, build_in_names[i],
+				ft_strlen(token->value)) == 0)
+			return (build_in_names[i]);
+		i++;
+	}
+	return (NULL);
 }
 
 void	ft_execution(t_token *tokens)
 {
-	t_token	*token;
-	char	*cmd;
+	t_token		*token;
+	const char	*cmd;
 
 	token = tokens;
 	while (token)
