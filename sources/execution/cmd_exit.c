1b1ac777360937f5c316ebe61d3a0ee0194316f7
KravchenkoYuliia <yuliiakravchenko10@gmail.com>
2025-06-13 16:10:47 +0200

    "exit" works exactly like in bash for arguments like this:
    -434
    +0000001
    -000009
    rtrtr (error message, returns 2)
    
    etc

--- sources/execution/cmd_exit.c
+++ sources/execution/cmd_exit.c
@@ -6,7 +6,7 @@
 /*   By: yukravch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 15:06:23 by yukravch          #+#    #+#             */
-/*   Updated: 2025/06/11 15:37:04 by yukravch         ###   ########.fr       */
+/*   Updated: 2025/06/13 16:08:43 by yukravch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ bool	ft_only_numeric(char *str)
 	int	i;
 
 	i = 0;
+	if (str[i] == '+' || str[0] == '-')
+		i = 1;
 	while (str[i])
 	{
 		if (!(str[i] >= '0' && str[i] <= '9'))
@@ -31,6 +33,7 @@ int	ft_exit(t_minishell *shell, int index)
 	if (shell->cmd[index]->args[1])
 	{
 		printf("exit\n");
+		shell->exit_status = ft_atoi(shell->cmd[index]->args[1]);
 		if (!ft_only_numeric(shell->cmd[index]->args[1]))
 		{
 			printf("%s: exit: %s: numeric argument required\n", SHELL_NAME_ERROR, shell->cmd[index]->args[1]);
@@ -42,7 +45,6 @@ int	ft_exit(t_minishell *shell, int index)
 			shell->exit_status = 1;
 			return (ERROR);
 		}
-		shell->exit_status = ft_atoi(shell->cmd[index]->args[1]);
 	}
 	exit((unsigned char)shell->exit_status);
 }
